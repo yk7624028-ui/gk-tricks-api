b1ff1a5fc99c9a375df1122e86b66897dd31f60f
yk7624028-ui <yk7624028@gmail.com>
2026-09-23 12:36:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ import uuid
 from functools import wraps
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from supabase import create_client, Client
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 # =========================================================
 # CONFIG
